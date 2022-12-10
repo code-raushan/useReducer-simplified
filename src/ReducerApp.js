2da50b7cfa0f773e5d472f4753925f58f8aebe1b
Raushan Kumar <raushan.development@gmail.com>
2022-12-10 17:42:56 +0530

    minor fix

--- src/ReducerApp.js
+++ src/ReducerApp.js
@@ -1,21 +1,22 @@
 import React, { useReducer } from "react";
 const initialState = 0;
-//reducer function takes two arguments - initial state and action. Returns the new state, that we get from useReducer() first result along with a dispatch method.
-const reducer = (initialState, action)=>{
+//reducer function takes two arguments - state that comes and action. Returns the new state, that we get from useReducer() first result along with a dispatch method.
+const reducer = (state, action)=>{
   switch(action.type){
     case 'increment':{
-      return initialState+1
+      return state+1
     }
     case 'decrement':{
-      return initialState-1;
+      return state-1;
     }
     default:{
-      return initialState
+      return state
     }
   }
 }
 
 const ReducerApp = () => {
+  //initially state is initialState
   const [state, dispatch]=useReducer(reducer, initialState)
   return (
     <div
